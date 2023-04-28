d6a667dfcb20b3765fb58fd06f8aa1f85cbd3d76
Kyunghwa Park <84437048+devhazel@users.noreply.github.com>
2023-04-28 15:09:42 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int max = -1;
-        int maxRow = 0;
-        int maxCol = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = -1, maxRow = 0, maxCol = 0;
         for(int i=1; i<=9; i++) {
+            String row = br.readLine();
+            StringTokenizer st = new StringTokenizer(row);
             for(int j=1; j<=9; j++) {
-                int num = scanner.nextInt();
-                if(max < num) {
+                int num = Integer.parseInt(st.nextToken());
+                if(num > max) {
                     max = num;
                     maxRow = i;
                     maxCol = j;
