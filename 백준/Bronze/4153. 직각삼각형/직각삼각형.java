e71f331c5c946de6e8b5e12075a671ee8d8c8989
Kyunghwa Park <84437048+devhazel@users.noreply.github.com>
2023-04-30 17:38:01 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] legs = new int[3];
-        while(true) {
-            for(int i=0; i<3; i++) {
-                legs[i] = scanner.nextInt();
-            }
-            if(legs[0]==0 && legs[1]==0 && legs[2]==0) {
-                break;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] line = br.readLine().split(" ");
+        while(!"0".equals(line[0])) {
+            int[] legs = {Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2])};
             Arrays.sort(legs);
-            if(Math.pow(legs[2], 2) == Math.pow(legs[0], 2) + Math.pow(legs[1], 2)) {
-                System.out.println("right");
+            if (Math.pow(legs[0], 2) + Math.pow(legs[1], 2) == Math.pow(legs[2], 2)) {
+                bw.write("right");
             } else {
-                System.out.println("wrong");
+                bw.write("wrong");
             }
+            bw.newLine();
+            line = br.readLine().split(" ");
         }
+        bw.flush();
+        br.close();
     }
 }
