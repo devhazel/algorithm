692823541615327573ce359d1c21f6a32e3a56a9
Kyunghwa Park <84437048+devhazel@users.noreply.github.com>
2023-05-24 16:39:58 +0900

    Create 시저 암호 개선.java
    메서드 분리

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호_개선.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            sb.append(pushAlphabet(c, n));
+        }
+        return sb.toString();
+    }
+
+    private char pushAlphabet(char c, int n) {
+        if(!Character.isAlphabetic(c)) {
+            return c;
+        }
+        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        return (char) ((c-offset+n)%26 + offset);
+    }
+}
