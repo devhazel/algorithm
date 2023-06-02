5733992125a4c655a21cf7003929f81d43e330db
devhazel <hazelpark.dev@gmail.com>
2023-06-03 00:57:12 +0900

    level 7. 재귀 > 거듭제곱 구하기

---
+++ 알고리즘잡스/lv7.basic_reculsive/거듭제곱 구하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), m = scanner.nextInt();
+        System.out.print(getPower(n, m));
+    }
+
+    // n^m을 반환하는 메서드
+    private static int getPower(int n, int m) {
+        if(m == 0) {
+            // 종료 조건 = 기저 조건
+            return 1;
+        } else {
+            // 점화식 : n^m = n * n^(m-1)
+            return n * getPower(n, m-1);
+        }
+    }
+}
